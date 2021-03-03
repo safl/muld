86d351afe7efbfb9f938799c8f1af567db5e605f
Simon A. F. Lund <os@safl.dk>
2021-03-03 12:52:14 +0100

    version-bump: v0.0.11
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*parts):
 
 setuptools.setup(
     name="muld",
-    version="0.0.10",
+    version="0.0.11",
     description="git; Mirror from Upstream to Local to Downstream",
     long_description=read('README.rst'),
     author="Simon A. F. Lund",
